3a70e194f5a279c83c73332cf781e7d721e398c4
sTeemAa <zloyadmdemon@gmail.com>
2025-05-14 15:30:02 +0300

    DZ3testMbLast

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -19,8 +19,20 @@ namespace UnitBrains.Player
             ///////////////////////////////////////
             // Homework 1.3 (1st block, 3rd module)
             ///////////////////////////////////////           
-            var projectile = CreateProjectile(forTarget);
-            AddProjectileToList(projectile, intoList);
+
+            if (GetTemperature() >= overheatTemperature)
+            {
+                return;
+            } else
+            {
+                for (int i = 0; i <= GetTemperature(); i++)
+                {
+                    var projectile = CreateProjectile(forTarget);
+                    AddProjectileToList(projectile, intoList);
+                }
+                IncreaseTemperature();
+            }
+
             ///////////////////////////////////////
         }
 
